a0d7c9bfb8a46d35b42f4e33cdc260bc76a239db
KorStrix <KorStrix@users.noreply.github.com>
2021-05-14 16:15:22 +0000

    Update file(s) "/." from "unity-korea-community/unity-utils"

---
+++ Assets/unity-utils/Tests/Runtime/DataSenderTests.cs
@@ -0,0 +1,55 @@
+using System;
+using NUnit.Framework;
+using UnityEngine;
+using UNKO.Utils;
+
+public class DataSenderTests
+{
+    public struct TestData
+    {
+        public string stringData;
+        public int numberData;
+
+        public TestData(int numberData)
+        {
+            this.stringData = numberData.ToString();
+            this.numberData = numberData;
+        }
+    }
+
+    public class TestSender : MonoBehaviour
+    {
+        public DataSender<TestData> sender { get; private set; } = new DataSender<TestData>();
+    }
+
+    public class TestReceiver : MonoBehaviour, IObserver<TestData>
+    {
+        public TestData data { get; private set; }
+
+        public void OnCompleted() { }
+        public void OnError(Exception error) { }
+
+        public void OnNext(TestData value)
+        {
+            this.data = value;
+        }
+    }
+
+    [Test]
+    public void 사용예시()
+    {
+        // Arrange
+        TestSender senderComponent = new GameObject(nameof(TestSender)).AddComponent<TestSender>();
+        TestReceiver receiverComponent = new GameObject(nameof(TestReceiver)).AddComponent<TestReceiver>();
+        receiverComponent.transform.SetParent(senderComponent.transform);
+        senderComponent.sender.InitChildrenComponents(senderComponent);
+
+        // Act
+        TestData testData = new TestData(UnityEngine.Random.Range(1, 100));
+        senderComponent.sender.SendData(testData);
+
+        // Assert
+        Assert.AreEqual(receiverComponent.data.stringData, testData.stringData);
+        Assert.AreEqual(receiverComponent.data.numberData, testData.numberData);
+    }
+}
